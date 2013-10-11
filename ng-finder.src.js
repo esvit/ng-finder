84fe40c1c9f5f38fec87bc9d9145910ea86ce5b1
Vitalii Savchuk <esvit666@gmail.com>
2013-10-11 16:28:18 +0300

    rebuild

--- ng-finder.src.js
+++ ng-finder.src.js
@@ -15,7 +15,7 @@ angular.module('ngFinder', [])
         return {
             restrict: 'C',
             scope: {
-                'file'      : '=',
+                'url'      : '=url',
                 'onSelect'  : '&select'
             },
             controller: ['$scope', function($scope) {
@@ -48,7 +48,7 @@ angular.module('ngFinder', [])
                         cwd    : menus.cwd.split(','),
                         files  : menus.files.split(',')
                     },
-                    url : attrs.url || '/elfinder',
+                    url : scope.url || '/elfinder',
                     commandsOptions : {
                         getfile: {
                             onlyURL: false
